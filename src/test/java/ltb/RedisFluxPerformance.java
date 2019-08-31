e4cccacf2bb785f93e063cd8ed22bc78454c0351
Colin Smith <colin.smith@gmail.com>
2019-08-31 12:22:19 -0700

    grid test for redis flux performance

--- src/test/java/ltb/RedisFluxPerformance.java
+++ src/test/java/ltb/RedisFluxPerformance.java
@@ -13,12 +13,14 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Queue;
 import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -62,24 +64,24 @@ public class RedisFluxPerformance {
     /**
      * run a reactive drain test with the given equipment and return a halted stopwatch
      * containing the elapsed time of the test, exclusive of data preparation and enqueueing.
-     *
+     * <p>
      * I admit, the mapper part is excessively tricky, but it was interesting to figure it
      * out. The goal is to allow the caller to select between a flatMap and concatMap
      * step. Each of those functions takes an A -> Flux&lt;A&gt; argument, is applied to
      * a flux, and produces a flux. Here we simplify by having the input and output types
      * the same, so the type of mapper is:
-     *
+     * <p>
      * (A -> Flux&lt;A&gt;) -> Flux&lt;A&gt; -> Flux&lt;A&gt;.
-     *
+     * <p>
      * Such a function can be supplied to a compose step in a reactive chain.
      *
-     * @param nItems How many MVarStubs to ship downstream.
+     * @param nItems       How many MVarStubs to ship downstream.
      * @param fluxSupplier how to transform Queue into Flux.
      * @param nConnections how many Redis connections in round robin pool (may be 1).
-     * @param mapper effectively, either a flatMap or concatMap step, in the form of a function.
-     *
+     * @param mapper       effectively, either a flatMap or concatMap step, in the form of a function.
      */
     private Stopwatch multiTest(
+            final String description,
             final int nItems,
             final Function<BlockingQueue<MVarStub>, Flux<MVarStub>> fluxSupplier,
             final int nConnections,
@@ -92,11 +94,13 @@ public class RedisFluxPerformance {
                 .limit(nItems)
                 .collect(Collectors.toCollection(LinkedBlockingQueue::new));
         final RRPool rrPool = new RRPool(client, nConnections);
-        Function<MVarStub, Publisher<MVarStub>> sender = stub -> send(stub, rrPool);
+        Function<MVarStub, Publisher<MVarStub>> sender = stub -> send(stub, description, rrPool);
         // I admit, this is probably overly tricky. The purpp
         // Function<Function<MVarStub, Publisher<MVarStub>>, Function<Flux<MVarStub>, Flux<MVarStub>>> mapper = suite -> flux -> flux.concatMap(suite);
 
+
         Stopwatch sw = Stopwatch.createStarted();
+
         fluxSupplier.apply(q)
                 //.publishOn(Schedulers.parallel())  // critical, it locks up if I do not call .publishOn()
                 //.filter(this::markers)
@@ -108,56 +112,84 @@ public class RedisFluxPerformance {
                 //.concatMap(sender)
                 .take(nItems)
                 .subscribe(
-                        stub -> {},
+                        stub -> {
+                        },
                         t -> fail("subscription", t),
                         semaphore::release
                 );
 
         semaphore.acquireUninterruptibly();
         sw.stop();
-        System.out.format("%s done in %s : %f/sec", nItems, sw, 1e9f * nItems / sw.elapsed(TimeUnit.NANOSECONDS));
         rrPool.shutdown();
         return sw;
     }
 
     @Test
     public void PlainQueue1ConnConcat() {
-        multiTest(64 * 1024, Flux::fromIterable, 1, sender -> (flux -> flux.concatMap(sender)));
+        multiTest("PQ1C", 64 * 1024, Flux::fromIterable, 1, sender -> (flux -> flux.concatMap(sender)));
     }
 
     @Test
     public void ReactiveQueue1ConnConcat() {
-        multiTest(64 * 1024, ReactiveQueueAdapter::of, 1, sender -> (flux -> flux.concatMap(sender)));
+        multiTest("RQ1C", 64 * 1024, ReactiveQueueAdapter::of, 1, sender -> (flux -> flux.concatMap(sender)));
     }
 
     @Test
     public void PlainQueue2ConnConcat() {
-        multiTest(64 * 1024, Flux::fromIterable, 2, sender -> (flux -> flux.concatMap(sender)));
+        multiTest("PQ2C", 64 * 1024, Flux::fromIterable, 2, sender -> (flux -> flux.concatMap(sender)));
     }
 
     @Test
     public void ReactiveQueue2ConnConcat() {
-        multiTest(64 * 1024, ReactiveQueueAdapter::of, 2, sender -> (flux -> flux.concatMap(sender)));
+        multiTest("RQ2C", 64 * 1024, ReactiveQueueAdapter::of, 2, sender -> (flux -> flux.concatMap(sender)));
     }
 
     @Test
     public void PlainQueue1ConnFlat() {
-        multiTest(64 * 1024, Flux::fromIterable, 1, sender -> (flux -> flux.flatMap(sender)));
+        multiTest("PQ1F", 64 * 1024, Flux::fromIterable, 1, sender -> (flux -> flux.flatMap(sender)));
     }
 
     @Test
     public void ReactiveQueue1ConnFlat() {
-        multiTest(64 * 1024, ReactiveQueueAdapter::of, 1, sender -> (flux -> flux.flatMap(sender)));
+        multiTest("RQ1F", 64 * 1024, ReactiveQueueAdapter::of, 1, sender -> (flux -> flux.flatMap(sender)));
     }
 
     @Test
     public void PlainQueue2ConnFlat() {
-        multiTest(64 * 1024, Flux::fromIterable, 2, sender -> (flux -> flux.flatMap(sender)));
+        multiTest("PQ2F", 64 * 1024, Flux::fromIterable, 2, sender -> (flux -> flux.flatMap(sender)));
     }
 
     @Test
     public void ReactiveQueue2ConnFlat() {
-        multiTest(64 * 1024, ReactiveQueueAdapter::of, 2, sender -> (flux -> flux.flatMap(sender)));
+        multiTest("RQ2F", 64 * 1024, ReactiveQueueAdapter::of, 2, sender -> (flux -> flux.flatMap(sender)));
+    }
+
+    /**
+     * Conduct a "grid" of tests over different payload counts, queue origins, mappers, and connection pool sizes.
+     * Produce output in CSV for analysis.
+     */
+
+    @Test
+    public void GridTest() {
+        List<Integer> nItemsKs = Arrays.asList(16, 32, 64, 128);
+        List<Tuple2<String, Function<BlockingQueue<MVarStub>, Flux<MVarStub>>>> fluxTypes = Arrays.asList(
+                Tuples.of("P", Flux::fromIterable),
+                Tuples.of("R", ReactiveQueueAdapter::of));
+        List<Integer> connectionCounts = Arrays.asList(1, 2, 3, 4);
+        List<Tuple2<String, Function<Function<MVarStub, Publisher<MVarStub>>, Function<Flux<MVarStub>, Flux<MVarStub>>>>> mappers = Arrays.asList(
+                Tuples.of("C", sender -> (flux -> flux.concatMap(sender))),
+                Tuples.of("F", sender -> (flux -> flux.flatMap(sender))));
+
+
+        nItemsKs.forEach(nItemsK ->
+                fluxTypes.forEach(fluxType ->
+                        connectionCounts.forEach(connCount ->
+                                mappers.forEach(mapper -> {
+                                    final String test = fluxType.getT1() + connCount + mapper.getT1();
+                                    final Stopwatch sw = multiTest(test, nItemsK * 1024, fluxType.getT2(), connCount, mapper.getT2());
+                                    System.out.format("%d,%s,%d,%s,%d\n", nItemsK * 1024, fluxType.getT1(), connCount, mapper.getT1(), sw.elapsed(TimeUnit.MILLISECONDS));
+                                    //System.out.format("%dk done in %s : %f/sec\n", nItemsK, sw, 1024. * nItemsK * 1e9f / sw.elapsed(TimeUnit.NANOSECONDS));
+                                }))));
     }
 
     @Test
@@ -189,7 +221,7 @@ public class RedisFluxPerformance {
         @Override
         public RedisReactiveCommands<String, String> get() {
             StatefulRedisConnection<String, String> c = conns.get(ix);
-            ix = (ix+1) % n;
+            ix = (ix + 1) % n;
             return c.reactive();
         }
 
@@ -198,13 +230,9 @@ public class RedisFluxPerformance {
         }
     }
 
-    private final String area = "Area51";
-
-    private Mono<MVarStub> send(MVarStub stub, Supplier<RedisReactiveCommands<String, String>> reactive) {
-
+    private Mono<MVarStub> send(MVarStub stub, String area, Supplier<RedisReactiveCommands<String, String>> reactive) {
         final String key = String.format("%s:%s", area, stub.varName);
         final String triplet = stub.triplet();
-
 
         return reactive.get().hset(key, triplet, stub.serialize())
                 .checkpoint("mvar-stubs-storage-2-send-1")
